7c797a76077eece1bcfe2c4200ce0f8c22416bf3
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-01 17:43:52 +0200

    esercizio grezzo completo

--- js/app.js
+++ js/app.js
@@ -16,27 +16,108 @@ const team = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
+        foto: "img/wayne-barnett-founder-ceo.jpg",
     },
     {
         nome: "Angela Carroll",
         ruolo: "Chief Editor",
+        foto: "img/angela-caroll-chief-editor.jpg",
     },
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
+        foto: "img/walter-gordon-office-manager.jpg",
     },
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
+        foto: "img/angela-lopez-social-media-manager.jpg",
     },
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
+        foto: "img/scott-estrada-developer.jpg",
     },
     {
         nome: "Barbara Ramos",
         ruolo: "Graphics Designer",
+        foto: "img/barbara-ramos-graphic-designer.jpg",
     }
 ];
 
 console.table(team);
+console.log(team[0].nome)
+
+const teamStamp=document.getElementById("teamContainer").innerHTML = 
+    `<div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[0].foto}"
+                alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[0].nome}</h3>
+            <p>${team[0].ruolo}</p>
+        </div>
+    </div>
+    <div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[1].foto}"
+                alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[1].nome}</h3>
+            <p>${team[1].ruolo}</p>
+        </div>
+    </div>
+    <div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[2].foto}"
+                alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[2].nome}</h3>
+            <p>${team[2].ruolo}</p>
+        </div>
+    </div>
+    <div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[3].foto}"
+                alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[3].nome}</h3>
+            <p>${team[3].ruolo}</p>
+        </div>
+    </div>
+    <div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[4].foto}"
+                alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[4].nome}</h3>
+            <p>${team[4].ruolo}</p>
+        </div>
+    </div>
+    <div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[5].foto}"
+                alt="Wayne Barnett"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[5].nome}</h3>
+            <p>${team[5].ruolo}</p>
+        </div>
+    </div>`
